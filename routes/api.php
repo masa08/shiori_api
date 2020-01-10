a73353e70480c922a140b6255356e4938113059c
masa08 <ushimasarug08@gmail.com>
2020-01-10 16:46:35 +0900

    :sparkles: Add CORS setting

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware(['cors'])->group(function () {
+    Route::post('login', 'ApiController@login');
+    Route::post('register', 'ApiController@register');
 
-Route::post('login', 'ApiController@login');
-Route::post('register', 'ApiController@register');
-
-Route::group(['middleware' => 'auth.jwt'], function () {
-    Route::get('logout', 'ApiController@logout');
+    Route::group(['middleware' => 'auth.jwt'], function () {
+        Route::get('logout', 'ApiController@logout');
+    });
 });
