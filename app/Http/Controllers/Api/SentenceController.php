887061aa1bef5da52be47a3e11b5da1ff6f9bb2a
masa08 <ushimasarug08@gmail.com>
2020-01-23 09:28:07 +0900

    :sparkles: Implement sentence store api

--- app/Http/Controllers/Api/SentenceController.php
+++ app/Http/Controllers/Api/SentenceController.php
@@ -5,7 +5,47 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Book;
+use App\Models\User;
+use App\Models\Sentence;
+
 class SentenceController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $title = $request->input('title');
+        $isbn = $request->input('isbn');
+        $author = $request->input('author');
+        $publisherName = $request->input('publisherName');
+        $itemCaption = $request->input('itemCaption');
+        $salesDate = $request->input('salesDate');
+        $largeImageUrl = $request->input('largeImageUrl');
+
+        if (empty(Book::where('isbn', $isbn)->first())) {
+            $book = new Book();
+            $book->title = $title;
+            $book->isbn = $isbn;
+            $book->author = $author;
+            $book->publisher_name = $publisherName;
+            $book->item_caption = $itemCaption;
+            $book->sales_date = $salesDate;
+            $book->large_image_url = $largeImageUrl;
+            $book->save();
+        }else{
+            $book = Book::where('isbn', $isbn)->first();
+        }
+
+        $request_sentence = $request->input('sentence');
+        $user_id = $request->input('user_id');
+        $user = User::find($user_id);
+        $sentence = new Sentence();
+        $sentence->sentence = $request_sentence;
+        $sentence->book_id = $book->id;
+        $sentence->user_id = $user->id;
+        $sentence->save();
+
+        return response()->json([
+            'sentence' => "success",
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,15 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware(['cors'])->group(function () {
+Route::group(['middleware' => 'cors'], function () {
     Route::post('login', 'Api\SessionsController@login');
     Route::post('register', 'Api\SessionsController@register');
+    Route::get('logout', 'Api\SessionsController@logout');
+    Route::options('sentence', 'Api\SentenceController@store');
+    Route::resource('sentence', 'Api\SentenceController', ['only' => ['store']]);
 
+    // TODO: JWTの再設定
     Route::group(['middleware' => 'auth.jwt'], function () {
-        Route::get('logout', 'Api\SessionsController@logout');
         Route::resource('users', 'Api\UserController', ['only' => ['show', 'update']]);
     });
 });
